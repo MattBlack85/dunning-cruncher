ba694ad4db481649c8e5a09fedae446ebb2c5383
MattBlack85 <promat85@gmail.com>
2013-08-14 11:07:09 +0200

    Updated some options for invoices, created a separated table to store vendors' informations, added some company codes."

--- core/models.py
+++ core/models.py
@@ -1,46 +1,60 @@
 from django.db import models
 
+class TblVendor(models.Model):
+      vname = models.CharField(max_length=50)
+      vnumber = models.IntegerField(max_length=11, unique=True)
+      vmail = models.EmailField(unique=True)
+
+      def __unicode__(self):
+            return '%s %s %s' %(self.vname, str(self.vnumber), str(self.vmail))
+
 class Data(models.Model):
+      MARKET_OPT = (
+            ('FI', 'Finland'),
+            ('DK', 'Denmark'),
+            ('SE', 'Sweden'),
+            ('NO', 'Norway'),
+            ('AT', 'Austria'),
+            ('FR', 'France'),
+            ('DE', 'Germany'),
+            ('IT', 'Italy'),
+            ('ES', 'Spain'),
+            ('PT', 'Portugal'),
+            ('PL', 'Poland'),
+            ('GB', 'Great Britain'),
+            ('CH', 'Switzerland'),
+       )
 
-	MARKET_OPT = (
-	    ('FI', 'Finland'),
-	    ('DK', 'Denmark'),
-	    ('SE', 'Sweden'),
-	    ('NO', 'Norway'),
-	    ('AT', 'Austria'),
-	    ('FR', 'France'),
-	    ('DE', 'Germany'),
-	    ('IT', 'Italy'),
-	    ('ES', 'Spain'),
-	    ('PT', 'Portugal'),
-	    ('PL', 'Poland'),
-	    ('GB', 'Great Britain'),
-	)
+       CCODE_OPT = (
+            ('10', '10'),
+            ('11', '11'),
+            ('13', '13'),
+            ('15', '15'),
+       )
 
-	CCODE_OPT = (
-	    ('X', 'X'),
-	)
+       LEVEL_OPT = (
+            ('1', '1st'),
+            ('2', '2nd'),
+            ('3', '3rd'),
+            ('4', '4th'),
+            ('9', 'to scan'),
+       )
 
-	LEVEL_OPT = (
-	    ('1', '1st'),
-	    ('2', '2nd'),
-	    ('3', '3rd'),
-	    ('4', '4th'),
-	    ('9', 'to scann'),
-	)
+       INVSTATUS_OPT = (
+            ('PA', 'Paid'),
+            ('RTV', 'RTV')
+            ('MI', 'Missing')
+       )
 
-	INVSTATUS_OPT = (
-	    ('X', 'X'),
-	)
-
-	market = models.CharField(max_length=3, choices=MARKET_OPT)
-	ccode = models.IntegerField(max_length=2, choices=CCODE_OPT)
-	level = models.CharField(max_length=8, choices=LEVEL_OPT)
-	apclerk = models.CharField(max_length=50)
-	actiondate = models.DateTimeField()
-	reminderdate = models.DateTimeField()
-	remindernumber = models.CharField(max_length=30)
-	vendor = models.IntegerField(max_length=11)
-	mailvendor = models.EmailField()
-	invoicenumber = models.IntegerField(max_length=20)
-	invoicestatus = models.CharField(max_length=100, choices=INVSTATUS_OPT)
+        market = models.CharField(max_length=3, choices=MARKET_OPT)
+        ccode = models.CharField(max_length=2, choices=CCODE_OPT)
+        level = models.CharField(max_length=8, choices=LEVEL_OPT)
+        apclerk = models.CharField(max_length=50)
+        actiondate = models.DateTimeField(auto_now_add=True)
+        reminderdate = models.DateTimeField()
+        remindernumber = models.CharField(max_length=30)
+        vendor = models.IntegerField()
+        mailvendor = models.EmailField()
+        invoicenumber = models.IntegerField(max_length=20)
+        invoicestatus = models.CharField(max_length=100, choices=INVSTATUS_OPT)
+        actiontaken=models.CharField(max_length=1000)
