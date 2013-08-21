124cac07dd0525d138d6e1d4832580602aff8e63
MattBlack85 <promat85@gmail.com>
2013-08-21 20:53:44 +0200

    Added urls to manage the login page and redirect if login is successful.

--- dunning_cruncher/urls.py
+++ dunning_cruncher/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from core import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+    url(r'^$', views.index),
+    url(r'^login/?$', views.login),
+    url(r'^main/?$', views.tracker),
     # url(r'^$', 'dunning_cruncher.views.home', name='home'),
     # url(r'^dunning_cruncher/', include('dunning_cruncher.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
+
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
