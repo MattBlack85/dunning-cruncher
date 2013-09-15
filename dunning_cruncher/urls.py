5116da2ab30495640474f16cdc7d4d05e1095136
MattBlack85 <promat85@gmail.com>
2013-09-15 20:07:29 +0200

    Added url to overview section.

--- dunning_cruncher/urls.py
+++ dunning_cruncher/urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
                        url(r'logout/?$', views.logout_view),
                        url(r'reporting/?$', views.reporting),
                        url(r'ajax/?$', views.ajax),
+                       url(r'overview/?$', views.edit),
     # url(r'^$', 'dunning_cruncher.views.home', name='home'),
     # url(r'^dunning_cruncher/', include('dunning_cruncher.foo.urls')),
 
