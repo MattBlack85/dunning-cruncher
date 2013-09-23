df60de71358710c3023b2bce920d2f23cf65dc8d
MattBlack85 <promat85@gmail.com>
2013-09-23 13:37:10 +0200

    Started the construction of edit item function.

--- dunning_cruncher/static/jquery/review.js
+++ dunning_cruncher/static/jquery/review.js
@@ -3,8 +3,34 @@ $(document).ready(function() {
 	var cleanclass = $(this).children('td:first').text().replace('/','').trim();
 	$(this).addClass(cleanclass);
     });
-    $('tr').on('click', function() {
+
+    $('tr.mainitems').on('click', function() {
 	var num = $(this).children("td:first").text().replace('/', '').trim();
 	$('.secondaryitems.' + num).toggle();
+    });
+
+    $(document).on('click', '.editbutton', function() {
+	//need to build an ajax call which will populate the modal with the data from the item
+	var objId = $(this).parent().parent().attr('id');
+	//setup the AJAX request
+	$.ajaxSetup({
+	    type: 'POST',
+	    dataType: 'json'
+	});
+
+	//the real AJAX request
+	$.ajax({
+	    url: '/ajax/',
+	    data: {
+		form_type: 'edit',
+		id: JSON.stringify(objId)
+	    },
+	    success: alert('Successfull'),
+	    error: function (ajaxObj, textStatus, error) {
+		alert(error);
+	    }
+	});
+	return true;
+	$('#modalreview').modal();
     });
 });
