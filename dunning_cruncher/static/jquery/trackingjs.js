d0535a848f6567aa3213334e73c54e39bf5abe1e
MattBlack85 <promat85@gmail.com>
2013-10-15 13:47:29 +0200

    Built a function to check if e-mail is e-mail like. The standard format is suppose to be john@doe.com.

--- dunning_cruncher/static/jquery/trackingjs.js
+++ dunning_cruncher/static/jquery/trackingjs.js
@@ -223,6 +223,12 @@ function SuccessfulTracking() {
 	$(this).val('');
     });
 
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+    return pattern.test(emailAddress);
+};
+
 function isInteger(value) {
     var intPattern = new RegExp(/^[0-9]+$/)
     return intPattern.test(value);
