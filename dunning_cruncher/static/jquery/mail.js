64227bcb7f65904b9c90e04a2a083cd89595c2d8
MattBlack85 <promat85@gmail.com>
2013-10-15 15:57:29 +0200

    Created another AJAX call to send a mail to buyer. The addres is written by hand and sended as JSON to the related view. The body is passed as JSON as well and loaded into the body of the mail.

--- dunning_cruncher/static/jquery/mail.js
+++ dunning_cruncher/static/jquery/mail.js
@@ -36,4 +36,30 @@ $(document).ready(function() {
     $("#mailbuttonbuy").on("click", function() {
 	$("#buyermod").modal();
     });
+
+    $("#buyermod").on("click", "button :last",  function() {
+	var mailTo = $("#modalmailemail").val();
+	var mailData = $("#modalmailbody").html();
+
+	//setup the AJAX request
+	$.ajaxSetup({
+	    type: 'POST',
+	    dataType: 'json'
+	});
+
+	//the real AJAX request
+	$.ajax({
+	    url: '/ajax/',
+	    data: {
+		form_type: "mailsendbuy",
+		mailbody: JSON.stringify(mailData),
+		mailto: JSON.stringify(mailTo),
+	    },
+	    //success: window.location.replace("/overview/"),
+	    error: function (ajaxObj, textStatus, error) {
+		alert(error);
+	    }
+	});
+	return true;
+    });
 });
