e484d70bbd688795b84dc0fffca37bd819895313
MattBlack85 <promat85@gmail.com>
2013-10-15 13:56:51 +0200

    This function basically creates a string with checked options. We loop through all elements of block "actioncheck" and if we get a positive value (checked option) we add the value of that choice to an array. When loop finishes we create a string from the array we got joining all the elements with "," character.

--- dunning_cruncher/static/jquery/trackingjs.js
+++ dunning_cruncher/static/jquery/trackingjs.js
@@ -228,8 +228,18 @@ $(document).ready(function(){
 function SuccessfulTracking() {
     alert("Item correctly tracked");
 };
+
+function actionArray() {
+    var actionPool = $(".actioncheck :input");
+    var actionList = new Array()
+
+    $.each(actionPool, function() {
+	if ($(this).prop("checked")) {actionList.push($(this).val())}
     });
 
+    actionList = actionList.join(",")
+    return actionList;
+};
 
 function isValidEmailAddress(emailAddress) {
     var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
