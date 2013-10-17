a35acbe749ac9db439e7992daf8ac792edd5aa85
MattBlack85 <promat85@gmail.com>
2013-10-17 15:35:59 +0200

    Eliminated attachment field. This field is now passive in our Engine model and will hold the id of the saved image on the server.

--- core/views.py
+++ core/views.py
@@ -111,7 +111,6 @@ def tracker (request):
     question.fields['amount'].widget.attrs = {'class': 'form-control'}
     question.fields['reasonother'].widget.attrs = {'class': 'form-control'}
     question.fields['actiontaken'].widget.attrs = {'class': 'form-control'}
-    #question.fields['attachment'].widget.attrs = {'class': 'form-control'}
     question.fields['actiondate'].widget.attrs = {
         'class': 'form-control',
         'value': date.today()
