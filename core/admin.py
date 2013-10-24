81ac1640e865f23115ee90d065d179e7a1f74b5d
MattBlack85 <promat85@gmail.com>
2013-10-24 14:37:37 +0200

    Changed the admin view of Engine Class.

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from core.models import Engine, Vendor
 
-admin.site.register(Engine)
+class EngineAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['remindernumber']}),
+        ('Date information', {'fields': ['actiondate'], 'classes': ['collapse']}),
+    ]
+    list_display = ('remindernumber', 'actiondate', 'is_done')
+    search_fields = ['remindernumber']
+    date_hierarchy = 'actiondate'
+
+admin.site.register(Engine, EngineAdmin)
 admin.site.register(Vendor)
