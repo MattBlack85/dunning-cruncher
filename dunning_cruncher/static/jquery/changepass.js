784f0b3c8f38b9b5e0aac6f724bc922257369f0f
MattBlack85 <promat85@gmail.com>
2013-11-28 09:28:30 +0100

    Created a really simple jQuery function to add a bootstrap class to form elements. We need to do that this way because we use django standard view to pass the form to the template so we can't (at least until we modify the view) add class before passing the form to the template.

---
+++ dunning_cruncher/static/jquery/changepass.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#id_old_password').addClass('form-control');
+    $('#id_new_password1').addClass('form-control');
+    $('#id_new_password2').addClass('form-control');
+});
