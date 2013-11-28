a93834d60f8535f2ab62036e7624c81322b0d619
MattBlack85 <promat85@gmail.com>
2013-11-28 09:18:17 +0100

    Added links for changing password.

--- dunning_cruncher/urls.py
+++ dunning_cruncher/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from core import views
 admin.autodiscover()
 
@@ -11,12 +12,13 @@ urlpatterns = patterns('',
                        url(r'^reporting/$', views.reporting),
                        url(r'^ajax/$', views.ajax),
                        url(r'^overview/$', views.edit),
+                       url(r'^password_change/$',
+                           auth_views.password_change,
+                           {'template_name': 'password_change_form.html'},
+                           name='password_change'),
+                       url(r'^password_change_done/$',
+                           auth_views.password_change_done,
+                           name='password_change_done'),
                        url(r'^(?P<drafttype>[a-z]{4})/(?P<dnumber>\d+)/(?P<language>[A-Z]{2})/$', views.draft),
-    # url(r'^$', 'dunning_cruncher.views.home', name='home'),
-    # url(r'^dunning_cruncher/', include('dunning_cruncher.foo.urls')),
-
-
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^admin/', include(admin.site.urls)),
 )
