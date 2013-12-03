891ecad5e6233641b2518821f6f30495d7fcb93f
MattBlack85 <promat85@gmail.com>
2013-12-03 22:13:20 +0100

    Added url to reminders section.

--- dunning_cruncher/urls.py
+++ dunning_cruncher/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+
 from core import views
+
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
                        url(r'^$', views.index),
@@ -21,5 +24,6 @@ urlpatterns = patterns('',
                            {'template_name': 'password_change_done.html'},
                            name='password_change_done'),
                        url(r'^(?P<drafttype>[a-z]{4})/(?P<dnumber>\d+)/(?P<language>[A-Z]{2})/$', views.draft),
+                       url(r'^reminders/(?P<year>\d{4})/(?P<week>\d{2})/$', views.tracking_calendar),
                        url(r'^admin/', include(admin.site.urls)),
 )
