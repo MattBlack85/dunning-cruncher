68a4ef87f410d0e65623da18e054a26e83edd4f8
MattBlack85 <promat85@gmail.com>
2013-12-05 16:10:33 +0100

    Added some jQuery to change the url and show different weeks basing on user needs.

--- dunning_cruncher/static/jquery/rcalendar.js
+++ dunning_cruncher/static/jquery/rcalendar.js
@@ -4,11 +4,28 @@ $(document).ready(function(){
 	$(this).attr('contentEditable', true);
     });
 
+    $('button').on('click', function() {
+	// Get the current addres and split every element into an array
+	var path = window.location.pathname.split('/');
+	var year = path[2]
+	var week = path[3]
+
+	if ( $(this).hasClass('next') ) {
+	    var where = parseInt(week)+1
+	} else if ( $(this).hasClass('prev') ) {
+	    var where = parseInt(week)-1
+	} else {
+	    alert("Wanna cheat?")
+	};
+
+	ChangeWeek(year, where);
+    });
+
     $('td').on('focusout', function() {
 	// Make the cell no editable
 	$(this).attr('contentEditable', false);
 
-	// Get the value just entered and the class (date) of the item
+	// Get the value just entered, the class (date) of the item and the relative header
 	var newVal = $(this).text();
 	var itemDate = $(this).attr('class');
 	var marketVal = $(this).closest('table').find('th').eq($(this).index()).text();
@@ -38,6 +55,9 @@ $(document).ready(function(){
 		alert(error);
 	    }
 	});
-
     });
 });
+
+function ChangeWeek(actualYear, actualWeek) {
+    window.location.replace('/reminders/'+actualYear+'/'+actualWeek+'/')
+};
