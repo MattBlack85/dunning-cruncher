f960a7a2fa29d246fe126f5cfc025654183633d3
MattBlack85 <promat85@gmail.com>
2013-12-09 16:30:21 +0100

    Added a general purpose module to store some useful structures.

---
+++ utils/func_utils.py
@@ -0,0 +1,16 @@
+class rem_to_do():
+    MCODE = {'DE': ['DE2', 'DE3', 'DE4', 'DEP2', 'DEP3', 'DEP4'],
+             'PL': ['PLW'],
+             'AT': ['AT'],
+             'NL': ['NL2', 'NL3', 'NL4'],
+             'IT': ['ITFP3L', 'ITW3L'],
+             'FR': ['FRPD', 'FRWD', 'FRD'],
+             'BE': ['BEF', 'BEW'],
+             'PT': ['PTD'],
+             'DK': ['DK2', 'DK3'],
+             'SE': ['SE2', 'SE3'],
+             'FI': ['FI2', 'FI3'],
+             'NO': ['NO2', 'NO3'],
+             'CH': ['CH2', 'CH3', 'CH4', 'CHW2', 'CHW3', 'CHW4']
+         }
+    
