27099aa5369b1d1907ed15a5bdc8af41facf3779
MattBlack85 <promat85@gmail.com>
2013-12-15 00:42:24 +0100

    Added firsts user test.

--- core/tests.py
+++ core/tests.py
@@ -1,13 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 from core.models import Engine
 
 from utils import tracking_utils
 
+from django.contrib.auth.models import User, Group
+
 import json
 
 import datetime
 
+class TestUser(TestCase):
+    @classmethod
+    def setUp(self):
+        # Let's create a dummy user for our tests.
+        create_test_user(self)
+
+class AnotherTest(TestUser):
+    def test_no_group(self):
+        self.assertEqual(self.testuser.groups.count(), 0)
+
+    def test_group_RU(self):
+        self.testuser.groups.add(1)
+        client = Client()
+        client.post('/login/', {'uname': 'testuser', 'passw':'password'})
+        response = client.get('/reporting/2013/49/')
+        self.assertEqual(self.testuser.groups.get().name, 'RU')
+        self.assertEqual(response.status_code, 403)
+
+
+    def destroy_group(self):
+        delete_group(self)
+        print self.testuser.groups.count()
+        self.assertEqual(self.testuser.groups.count(), 0)
+        
+        
 
 class EngineTest(TestCase):
 
@@ -38,3 +65,13 @@ class EngineTest(TestCase):
         item.save()
 
         self.assertNotEqual(item.pk, None)
+
+def create_test_user(cls):
+    cls.testuser = User.objects.create_user('testuser', 'test@test.com', 'password')
+    Group.objects.create(name='RU')
+
+def delete_test_user(cls):
+    User.objects.filter(username='testuser').delete()
+
+def delete_group(cls):
+    Group.objects.all().delete()
