14825be4ba704d609d1a9f0ec1a924833f6deb38
MattBlack85 <promat85@gmail.com>
2013-12-16 11:36:15 +0100

    Added some jQuery to count the balance for every country.

--- dunning_cruncher/static/jquery/reports.js
+++ dunning_cruncher/static/jquery/reports.js
@@ -1,11 +1,37 @@
 $(document).ready(function() {
-    var rowSaldo = $('.saldo')
+    var rowBalance = $('.balance')
+    var path = window.location.pathname.split('/')
+    var year = path[2]
+    var month = path[3]
 
-    $.each(rowSaldo, function() {
+    $('#monthchoice').val(month);
+
+    $.each(rowBalance, function() {
 	var index = $(this).index()-1;
 	var thiscountrydone = parseInt($('.alldone').eq(index).text().trim());
 	var thiscountrytodo = parseInt($('.todo').eq(index).text().trim());
+	var thiscountrybacklog = parseInt($('.backlog').eq(index).text().trim());
 
 	$(this).text(thiscountrydone-thiscountrytodo);
+
+	if ( parseInt($(this).text()) < 0 ) {
+	    $(this).addClass('negative');
+	};
+    });
+
+    $('#monthchoice,#yearchoice').on('change', function() {
+	if ( $(this).attr('id') == 'monthchoice' ) {
+	    var cMonth = $(this).val();
+	    ChangePeriod(year, cMonth);
+	} else if ( $(this).attr('id') == 'yearchoice' ) {
+	    var cYear = $(this).val();
+	    ChangePeriod(cYear, month);
+	} else {
+	    alert('Wanna cheat uh?');
+	};
     });
 });
+
+function ChangePeriod(cYear, cMonth) {
+    window.location.replace('/reporting/'+cYear+'/'+cMonth+'/')
+};
